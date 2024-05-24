a21c4d26b8072f1aa7663dcf69aef1054dd12007
Xuskia11 <atuka.09.123@gmail.com>
2024-05-24 06:13:25 -0700

    first commit

---
+++ practice/dictionaries.py
@@ -0,0 +1,20 @@
+# how to use vacebulary and how it works
+
+dct = { "Name" : "John" ,
+    "Age" : 25 ,
+ "Height" : 6.1 }
+
+print(dct[ "Name" ]) #John
+print (dct[ "Age" ]) # 25
+print (dct[ "Height" ]) # 6.1
+
+# DICTIONARY FUNCTIONS
+print(len(dct)) # 3
+print(str(dct)) #{'Name': 'John', 'Age': 25, 'Height': 6.1}
+print(dct.clear()) # remove all elemnts in vacebulary
+print(dct.copy()) #  Returns a copy of the dictionary
+print(dct.get())
+print(dct.has_key())
+ 
+ 
+ 
