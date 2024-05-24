b598a7b48aebd06d9db12e65daa586248fbbbadf
Xuskia11 <atuka.09.123@gmail.com>
2024-05-24 06:23:35 -0700

    commit

--- python begginer/practice/dictionaries.py
+++ python begginer/practice/dictionaries.py
@@ -1,8 +1,8 @@
 # how to use vacebulary and how it works
 
 dct = { "Name" : "John" ,
-    "Age" : 25 ,
- "Height" : 6.1 }
+        "Age" : 25 ,
+        "Height" : 6.1 }
 
 print(dct[ "Name" ]) #John
 print (dct[ "Age" ]) # 25
@@ -13,8 +13,8 @@ print(len(dct)) # 3
 print(str(dct)) #{'Name': 'John', 'Age': 25, 'Height': 6.1}
 print(dct.clear()) # remove all elemnts in vacebulary
 print(dct.copy()) #  Returns a copy of the dictionary
-print(dct.get())
-print(dct.has_key())
+print(dct.get()) # return one of key value 
+print(dct.has_key()) #return key if it is in or not
  
  
  
